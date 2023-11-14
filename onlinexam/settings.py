10af7b25af252b55d9a0ddf26655f28abdbde0aa
Mahesh <thakkilm@mail.gvsu.edu>
2023-11-14 10:30:49 -0500

    Add background images

--- onlinexam/settings.py
+++ onlinexam/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '@k0#p3kidu)yaaa3u1hplxz)f@^6xiy384*(+n@@s5x#1bx@m5'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['yourdomain.com','3.85.170.49']
+ALLOWED_HOSTS = ['yourdomain.com','3.85.170.49','localhost','127.0.0.1']
 
 
 # Application definition
